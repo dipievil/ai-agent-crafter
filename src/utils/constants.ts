89c6655dd471b2801a2e54e135edaa2cf8c8f15e
dipi evil <dipi.evil@gmail.com>
2026-03-03 11:41:19 -0300

    fix: add missing files

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+import { FileTypes, FileType } from "../app/components/wizard.types";
+
+export const CONTINUE_EMOJI = "➡️";
+
+export const fileOptions: FileTypes = {
+  ["agent-instructions"]: "agentInstructions",
+  ["specific-instructions"]: "specificInstructions",
+  ["prompts"]: "prompts",
+  ["skills"]: "skills"
+};
